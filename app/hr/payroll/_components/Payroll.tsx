b94f3faf892ce6d2b83cc5f2864b3a179c65193c
Frozen-369 <frozenboyarp@gmail.com>
2024-07-19 13:29:46 +0545

    Payroll Management Started

---
+++ app/hr/payroll/_components/Payroll.tsx
@@ -0,0 +1,14 @@
+"use client";
+import HRSidebar from '@/app/components/HRSidebar/hrsidebar';
+import React, { useState, useEffect } from 'react'
+
+const Payroll = () => {
+  return (
+    <div className="flex h-screen">
+        <HRSidebar/>        
+    </div>
+    
+  )
+}
+
+export default Payroll
---
+++ app/hr/payroll/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Payroll from './_components/Payroll'
+
+const page = () => {
+  return (
+    <Payroll/>
+  )
+}
+
+export default page
