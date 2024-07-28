dacab0585cb043fbb30d04dff8dd5b89e92a9b13
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-07-29 00:29:06 +0545

    Changes

--- app/inventory/entry/page.tsx
+++ app/inventory/entry/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { PurchaseEntrySlip } from "@/app/components/PurchaseEntryForm/PurchaseEntryForm";
 import React from "react";
 import { usePathname } from "next/navigation";
@@ -6,7 +7,6 @@ import { AuthorizationWrapper } from "@/app/util/authContext";
 
 const Page = ({ params }: { params: { orderId: string } }) => {
   const pathname = usePathname();
-
   return (
     <AuthorizationWrapper pathname={pathname}>
       <PurchaseEntrySlip orderId={params.orderId} />
