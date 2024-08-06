b4e95cb380324d9f1ac8d6eee3b6270894bdfb69
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-08-06 14:41:41 +0545

    Added Screen Fix in Holiday Page

--- app/hr/holidays/_components/Holidays.tsx
+++ app/hr/holidays/_components/Holidays.tsx
@@ -175,7 +175,7 @@ const HolidaysPage: React.FC = () => {
   };
 
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <HRSidebar />
       <main className="flex-1 p-6">
         <h1 className="text-2xl font-bold mb-6">Holidays</h1>
